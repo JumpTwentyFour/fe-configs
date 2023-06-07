3ff5f5527a1496748212dbfc92c2e7ef76b98f4e
Daniel Jarrett <daniel.jarrett@jump24.co.uk>
2023-06-07 09:15:01 +0100

    typescript test

--- packages/eslint/vue/index.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require('eslint-define-config')
-
-module.exports = defineConfig({
-  root: true,
-  extends: ['@jump24/eslint-config-typescript', 'plugin:vue/vue3-recommended'],
-  rules: {},
-})
